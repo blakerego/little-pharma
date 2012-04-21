54e7a6bc06189e568a25fb1b361b220df28aaaf6
blake <blake.rego@gmail.com>
2012-04-21 16:01:20 -0400

    Initial commit for little pharma

---
+++ littlepharma_rails/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LittlepharmaRails::Application.config.secret_token = 'e27529af81dbca29c76cc22f431bcdf0d491ee1aa0e8974f9930fb164a8b00c3a8983ca53c9db35a0c48273adc3935b8f6114433ca422f38e2867543e8c15f51'
